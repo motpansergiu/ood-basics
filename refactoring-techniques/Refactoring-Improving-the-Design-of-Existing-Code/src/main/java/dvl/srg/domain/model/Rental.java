f67c2c701263bbed65a576e00313a7f297d110c0
Serghei Motpan <Serghei.Motpan@endava.com>
2018-02-08 17:18:50 +0200

    Initial Code (whithout any refactors)

---
+++ refactoring-techniques/Refactoring-Improving-the-Design-of-Existing-Code/src/main/java/dvl/srg/domain/model/Rental.java
@@ -0,0 +1,19 @@
+package dvl.srg.domain.model;
+
+public class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+}
